1f436d76e663073794aa5da6aa67834bf630d7a1
fora <fora@technodom.kz>
2023-12-03 23:17:12 +0600

    tests with draggable views

--- app/src/main/java/kz/dkgroup/lexdialogsheet/MainActivity.kt
+++ app/src/main/java/kz/dkgroup/lexdialogsheet/MainActivity.kt
@@ -3,14 +3,21 @@ package kz.dkgroup.lexdialogsheet
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.DraggableState
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -20,17 +27,32 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusOrder
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kz.dkgroup.lexdialogsheet.library.LexDialogSheet
+import kz.dkgroup.lexdialogsheet.library.rememberLexDialogSheetState
 import kz.dkgroup.lexdialogsheet.ui.theme.LexDialogSheetTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LexDialogSheetTheme {
+                val focusRequester = FocusRequester()
+                LaunchedEffect(Unit) {
+                    focusRequester.requestFocus()
+                }
+                BasicTextField("", {}, modifier = Modifier.focusRequester(focusRequester))
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -43,17 +65,39 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+//@OptIn(ExperimentalMaterial3Api::class)
+//@Composable
+//fun BottomSheetWithHeaderAndFooter(name: String, modifier: Modifier = Modifier) {
+//    val sheetState = rememberModalBottomSheetState()
+//
+//    LaunchedEffect(Unit) {
+//        sheetState.show()
+//    }
+//    ModalBottomSheet(onDismissRequest = {}, sheetState = sheetState) {
+//        Text("Header")
+//        Column(modifier = Modifier.fillMaxWidth().verticalScroll(rememberScrollState())) {
+//            repeat(100) {
+//                Text("Body")
+//            }
+//        }
+//        Text("Footer")
+//    }
+//}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BottomSheetWithHeaderAndFooter(name: String, modifier: Modifier = Modifier) {
-    val sheetState = rememberModalBottomSheetState()
+    val sheetState = rememberLexDialogSheetState()
 
     LaunchedEffect(Unit) {
-        sheetState.show()
+//        sheetState.show()
     }
-    ModalBottomSheet(onDismissRequest = {}, sheetState = sheetState) {
+    LexDialogSheet(sheetState = sheetState) {
         Text("Header")
-        Column(modifier = Modifier.fillMaxWidth().verticalScroll(rememberScrollState())) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+        ) {
             repeat(100) {
                 Text("Body")
             }
@@ -67,5 +111,38 @@ fun BottomSheetWithHeaderAndFooter(name: String, modifier: Modifier = Modifier) 
 fun GreetingPreview() {
     LexDialogSheetTheme {
         BottomSheetWithHeaderAndFooter("Android")
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DraggableSample() {
+    LexDialogSheetTheme {
+        Box(modifier = Modifier.fillMaxSize()) {
+            var moveState = remember {
+                mutableFloatStateOf(0f)
+            }
+            Column(modifier = Modifier
+                .size(200.dp)
+                .graphicsLayer {
+                    translationY = moveState.floatValue
+                }
+                .background(Color.Yellow)
+                .draggable(
+                    state = DraggableState { delta ->
+                        println("delta $delta")
+                        moveState.floatValue += delta
+                    },
+                    orientation = Orientation.Vertical,
+                    onDragStopped = { velocity ->
+                        println("velocity $velocity")
+                    }
+                )) {
+                repeat(100) {
+                    Text("Body")
+                }
+            }
+        }
     }
 }
--- app/src/main/java/kz/dkgroup/lexdialogsheet/library/LextDialogSheet.kt
+++ app/src/main/java/kz/dkgroup/lexdialogsheet/library/LextDialogSheet.kt
@@ -1,20 +1,52 @@
 package kz.dkgroup.lexdialogsheet.library
 
+import android.util.Log
+import androidx.compose.foundation.gestures.DraggableState
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Velocity
 import androidx.compose.ui.unit.dp
 import kotlin.math.roundToInt
 
+@Composable
+fun rememberLexDialogSheetState(): LexDialogSheetState {
+    return remember {
+        LexDialogSheetState()
+    }
+}
 
 class LexDialogSheetState(
     val anchors: List<LexDialogAnchor> = LexDialogAnchor.DEFAULT,
-    direction: Direction = Direction.Bottom
+    val initialAnchor: LexDialogAnchor = LexDialogAnchor.Hidden,
+    val direction: Direction = Direction.Bottom
 ) {
+//
+//    internal var swipeableState = SwipeableV2State(
+//        initialValue = initialAnchor,
+//        animationSpec = SwipeableV2Defaults.AnimationSpec,
+//        confirmValueChange = confirmValueChange,
+//    )
 
+}
+
+fun LexDialogSheetState.dragDirection() = when (direction) {
+    Direction.Bottom -> Orientation.Vertical
+    Direction.Top -> Orientation.Vertical
+    Direction.Left -> Orientation.Horizontal
+    Direction.Right -> Orientation.Horizontal
 }
 
 @Composable
@@ -34,8 +66,57 @@ fun LexDialogSheet(
                 is LexDialogAnchor.Percent -> LexDialogAnchor.Px(it.value * fullHeight / 100)
             }
         }.sortedBy { it.value }
-        LexDialogSheetLayout(sheetState = sheetState, modifier = Modifier.fillMaxSize()) {
-            content()
+        Surface(modifier = Modifier
+            .fillMaxSize()
+            .draggable(DraggableState {
+
+            }, orientation = sheetState.dragDirection(), onDragStopped = { velocity ->
+                Log.e("LDSScrollConnection", "velocity $velocity")
+            })
+            .nestedScroll(
+                remember(sheetState) {
+                    LexNestedScrollConnection()
+                }
+            )) {
+            Column {
+                Box {
+                    pinStart()
+                }
+                content()
+                Box {
+                    pinEnd()
+                }
+            }
         }
+    }
+}
+
+class LexNestedScrollConnection : NestedScrollConnection {
+    override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
+        val velocity = super.onPostFling(consumed, available)
+        Log.e("LDSScrollConnection", "velocity fling $velocity")
+        return velocity
+    }
+
+    override fun onPostScroll(
+        consumed: Offset,
+        available: Offset,
+        source: NestedScrollSource
+    ): Offset {
+        val offset = super.onPostScroll(consumed, available, source)
+        Log.e("LDSScrollConnection", "offset $offset")
+        return offset
+    }
+
+    override suspend fun onPreFling(available: Velocity): Velocity {
+        val velocity = super.onPreFling(available)
+        Log.e("LDSScrollConnection", "velocity $velocity")
+        return velocity
+    }
+
+    override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+        val offset = super.onPreScroll(available, source)
+        Log.e("LDSScrollConnection", "offset scrollD $offset")
+        return offset
     }
 }
